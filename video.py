7090c4dadba415662f23d0bdeaf753bb6c584e87
DoomSkull <dfordoomskull@gmail.com>
2023-02-06 11:52:40 +0000

    Video of evolution

---
+++ video.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+import os
+
+data_dir = "cmake-build-debug/output/"
+
+files = os.listdir(data_dir)
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(projection='3d')
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+
+def animate(i):
+    print(i)  # because it's slow af and i wanna know where it is
+    ax.clear()
+    data = np.loadtxt(data_dir+str(i)+'.txt')
+    for point in data:
+        ax.scatter(point[0], point[1], point[2], color='red' if point[3] == 1 else 'blue', s=50)
+    return ax,
+
+
+ani = animation.FuncAnimation(fig, animate, frames=len(files), interval=33.333333, blit=False)
+writervideo = animation.FFMpegWriter(fps=30)
+ani.save("lattice evolution.mp4", writer=writervideo)
+plt.close()
